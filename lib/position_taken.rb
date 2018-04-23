62e22a80dba4885c63a0249498957638fb50d2fc
Lorena <lorena.giddens@outlook.com>
2018-04-23 17:19:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return FALSE
-    elsif board[index] == "X" || "O"
-      return TRUE
+  elsif board[index] == "X" || "O"
+    return TRUE
   end
 end
