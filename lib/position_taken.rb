91604030bb9eae5c7b66bb9faa719fc2a849ec34
Lorena <lorena.giddens@outlook.com>
2018-04-23 17:41:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,11 @@
 # code your #position_taken? method here!
 
-
 def position_taken?(board, index)
-  if (board[index] == " " || "" || nil)
+  if (board[index] == " ") || 
+     (board[index] == "") ||
+     (board[index] == nil)
     false
-  else (board[index] == "X" || "O")
+  else
     true
   end
 end
